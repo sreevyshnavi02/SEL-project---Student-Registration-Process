14b45677ea827b782d9f6d8848f4cc103b750f15
Sree Vyshnavi <sreevysh2002@gmail.com>
2023-04-02 13:24:38 +0530

    Fetching the reqd courses

--- OEC_allotment/preregister.php
+++ OEC_allotment/preregister.php
@@ -9,33 +9,43 @@
 <body>
     <!-- check if the student is btwn 4th to 7th sem -->
     <?php 
-        include '../header.php';
+        include '../header.php'; 
+        $year =  $_POST['session_year'];
+        $formatted_year = $year[strlen($year) - 2].$year[strlen($year) - 1];
+        $_SESSION['chosen_session'] = $formatted_year.$_POST['session_month'];
+
+
         $regno = $_SESSION['regno'];   //entered during login
         //-------temporary--------
         //to get the session as input from the user
-        $session = $_POST['session']; 
+        $session = $_SESSION['chosen_session']; 
 
         $stud_sem = $conn -> query ("select * from u_student where regno = '$regno';");
         $sem = $stud_sem -> fetchAll(PDO::FETCH_ASSOC);
-        if(1 || $sem[0]['CURR_SEM'] >= 4 && $sem[0]['CURR_SEM'] <= 7){
+        if($sem[0]['CURR_SEM'] >= 4 && $sem[0]['CURR_SEM'] <= 7){
             //eligible to register for oec
             //display a drop down menu with the available oecs
 
             //check if the capacity is reached for an oec before displaying it
 
-            $fetch_oec = $conn -> query("select e.course_code, c.course_name, c.course_type 
-            from u_prgm_elective_course e 
-            inner join u_course c 
-            on e.no_of_students_enrolled < e.capacity 
-            and e.session = '$session' 
-            and c.course_type = 'OEC';");
+            $fetch_oec_sql = "select e.course_code, c.course_name, c.course_type 
+            from u_prgm_elective_course e, u_course c 
+            where e.no_of_students_enrolled < e.capacity 
+            and e.session = :sess and e.course_code = c.course_code";
 
-            $fetch_stud_details = $conn -> query("select sname, curr_sem, credits_earned from u_student where regno = '$regno';");
-            $n = $fetch_stud_details->fetch(PDO::MYSQL_ATTR_FOUND_ROWS);
-            echo($n);
+            $fetch_oec_query = $conn -> prepare($fetch_oec_sql);
+            $fetch_oec_query -> bindParam(':sess', $session);
+            $fetch_oec_query -> execute();
 
-            $stud_data = $fetch_stud_details -> fetchAll(PDO::FETCH_ASSOC);
-            print_r($stud_data);
+            $fetch_oec = $fetch_oec_query -> fetchAll(PDO::FETCH_ASSOC);
+
+            $fetch_stud_details_sql = "select sname, curr_sem, credits_earned from u_student where regno = :regno";
+            $fetch_stud_details = $conn -> prepare($fetch_stud_details_sql);
+            $fetch_stud_details -> bindParam(':regno', $regno);
+            $fetch_stud_details -> execute();
+            $n = $fetch_stud_details -> rowCount();
+
+            $fetched_stud_data = $fetch_stud_details -> fetchAll(PDO::FETCH_ASSOC);
 
             //display the rows fetched in this query in drop-down menu
         ?>
@@ -43,24 +53,29 @@
         <div class="oec_option_div">
             <!-- display student details -->
             <form action="preregister_submit.php" method = "post">
-                <label for="sname">Name: </label>
-                <input type="text" name="sname" id="sname" value="<?php echo $stud_data['sname']; ?>" readonly>
+                <?php 
+                foreach($fetched_stud_data as $stud_data){ 
+                    echo '
+                        <label for="sname">Name: </label>
+                        <input type="text" name="sname" id="sname" value="'.$stud_data['sname'] .'" readonly>
 
-                <label for="curr_sem">Semester: </label>
-                <input type="text" name="curr_sem" id="curr_sem" readonly value="<?php echo $stud_data['curr_sem'] ?>">
+                        <label for="curr_sem">Semester: </label>
+                        <input type="text" name="curr_sem" id="curr_sem" readonly value="'.$stud_data['curr_sem'] .'">
 
-                <label for="credits_earned">Credits earned: </label>
-                <input type="text" name="credits_earned" id="credits_earned" readonly value="<?php echo $stud_data['credits_earned'] ?>">
+                        <label for="credits_earned">Credits earned: </label>
+                        <input type="text" name="credits_earned" id="credits_earned" readonly value="'.$stud_data['credits_earned'] .'">
+                    ';
+                }
 
-                <select name="oec_choose" id="oec_choose">
-                    <?php
-                    foreach($fetch_oec as $oec){
-                        echo("inside foreach");
-                        print_r($oec);
-                        echo("<option value='".$oec['course_code']."'>".$oec['course_code']." - ".$oec['course_name']."</option>");
-                    }
-                    ?>
-                </select>
+                echo '<select name="oec_choose" id="oec_choose">';
+                foreach($fetch_oec as $oec){
+                    echo('<option value="'.$oec["course_code"].'">'.$oec["course_code"].' - '.$oec["course_name"].'</option>');
+                }
+                echo '</select>';
+
+                ?>
+
+                <button type="submit">Confirm Option</button>
             </form>
         </div>
     <?php
